ecf1b2373099f7b9197993d00d11166c9c0f3b29
Martin <mbunger87@gmail.com>
2022-01-22 21:59:52 +0000

    Initial price and score code

--- data/scraper.js
+++ data/scraper.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
+const fs = require('fs');
 
 const getPriceUrl = (product_name) => `https://www.pricerunner.dk/cl/37/Grafikkort?q=${product_name}`;
 const getScoreUrl = () => `https://www.videocardbenchmark.net/high_end_gpus.html`;
+const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+const gpuList = ['GeForce GTX 1660 SUPER', 'GeForce RTX 3090', 'Radeon RX 6900 XT', 'Radeon RX 6800 XT'];
 
 // Idea https://www.youtube.com/watch?v=XTAYT37UuEY&ab_channel=CodingGarden
 async function getPrices(product_name) {
@@ -10,11 +13,63 @@ async function getPrices(product_name) {
     const { data: html } = await axios.get(productUrl, {
         // Mimic browser headers
         headers: {
-            Accept: 'hej'
+            authority: 'www.pricerunner.dk',
+            scheme: 'https',
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'cache-control': 'no-cache',
+            pragma: 'no-cache',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
         }
     });
     const dom = new JSDOM(html);
-    console.log(dom.window.document.querySelector('.SnarOLmYcb Ci0mGVkzmW CoU3uHMRiq css-1msxq8e').textContent)
+    const list = dom.window.document.querySelectorAll('.SnarOLmYcb.Ci0mGVkzmW.CoU3uHMRiq.css-1msxq8e');
+    const priceList = [];
+    list.forEach(price => {
+        priceList.push(parseInt(price.textContent.split(" ")[0].replace('.','')));
+    });
+    console.log(priceList);
+    return {
+        min: Math.min(...priceList),
+        max: Math.max(...priceList),
+        avg: parseInt(average(priceList).toFixed(0)),
+        time: new Date()
+    }
 }
 
-getPrices('GeForce GTX 1660 SUPER');
+async function getScore(product_name) {
+    const { data: html } = await axios.get(getScoreUrl(), {
+        headers: {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+        }
+    });
+
+    const dom = new JSDOM(html);
+    let score;
+    for (const span of dom.window.document.querySelectorAll(".prdname")) {
+        if (span.textContent.includes(product_name)) {
+            const nextSibling = span.nextElementSibling;
+            const nextSibling2 = nextSibling.nextElementSibling;
+            score = parseInt(nextSibling2.textContent.replace(',',''));
+            break;
+        }
+    }
+    return score;
+}
+
+// Load .json
+// let rawdata = fs.readFileSync('data/score.json');
+// const score = JSON.parse(rawdata);
+// console.log(score)
+getScore('GeForce GTX 1660 SUPER').then(score => {
+    console.log(score);
+});
+
+// gpuList.forEach(gpu => {
+//     console.log('Fetching values for: ' + gpu);
+//     getPrices(gpu).then((price) => {
+//         console.log('Writing file');
+//         let data = JSON.stringify(price);
+//         fs.writeFileSync('data/' + gpu.replaceAll(' ','') + '.json', data);
+//     });
+// });
